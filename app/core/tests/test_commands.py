8a39d975d55f03bafe35eb3d41fc1f795621a73a
DenSznk <densznk@gmail.com>
2023-02-09 14:11:33 +0400

    fix test and linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,7 +8,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-
+ 
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -20,7 +20,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -31,5 +31,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
-        
+        patched_check.assert_called_with(databases=['default'])
