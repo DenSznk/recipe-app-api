2bbef4609563a32916efeb9aebd22b88b466aa18
DenSznk <densznk@gmail.com>
2023-02-09 09:46:52 +0400

    add wait for db and test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,13 +14,13 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
-    # def test_wait_for_db_ready(self, patched_check):
-    #     """Test waiting for database if database ready."""
-    #     patched_check.return_value = True
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
 
-    #     call_command('wait_for_db')
+        call_command('wait_for_db')
 
-    #     patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_once_with(database=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -31,4 +31,5 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(databases=['default'])
+        patched_check.assert_called_with(database=['default'])
+        
